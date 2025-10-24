712ba94bf11d94cf1ead13211e529cca1798cf34
An <anthunguyen03@gmail.com>
2025-10-23 17:36:54 -0700

    Fix the Detector. Now work in real-time
    Merge OpenCV from capture_video.py to read the frame

--- hand_tracking.py
+++ hand_tracking.py
@@ -2,15 +2,20 @@ import os
 import mediapipe as mp
 from mediapipe.tasks import python
 from mediapipe.tasks.python import vision
+import cv2
+import time
+
+detector = None
+hand_landmarker_result = None
 
 def print_result(result: mp.tasks.vision.HandLandmarkerResult, output_image: mp.Image, timestamp_ms: int):
+    hand_landmarker_result = result
     print("Hand Landmarker Result: {}".format(result))
 
-def setup_hand_landmarker():
+def main():
     """
-    Initialize and return a MediaPipe Hand Landmarker detector
+    1. Initialize and return a MediaPipe Hand Landmarker detector
     """
-
     ## Store model path for the Hand Landmark task ##
     BASE_DIR = os.path.dirname(os.path.abspath(__file__))
     model_path = os.path.join(BASE_DIR, "model", "hand_landmarker.task")
@@ -31,26 +36,72 @@ def setup_hand_landmarker():
     options = HandLandmarkerOptions(base_options=BaseOptions(model_asset_path=model_path),
                                         running_mode=VisionRunningMode.LIVE_STREAM, result_callback = print_result) 
     detector = HandLandmarker.create_from_options(options)
-    return detector
 
-def detect_hand_landmark():
-    """
-    Read the frame from webcam and run the detection
-    """
 
-    ## Load Webcam ##
     """
-    TO-DO: 
-    1. Read frame from OpenCV 
-    2. Convert frame received from Webcam to a MediaPipe's Image object
-    3. Send live image data to perform hand landmarks detection
+    2. Initialize and Run OpenCV to Open Webcam
     """
+    # Create a VideoCapture object and Use the default camera
+    cam = cv2.VideoCapture(0)
 
-    ## Detect hand landmarks from the input real-time webcam ##
-    # hand_landmarker_result = landmarker.detect_async(mp_image, frame_timestamp_ms)
+    # Check if the camera actually opened succesfully
+    if not cam.isOpened():
+        print("Cannot open camera. Exiting...")
+        exit()
 
-def main():
-    detector = setup_hand_landmarker()
+    while True:
+        # Read a single frame from the camera
+        # ret = Return value -> True if frame read succesfully, else return False
+        # frame = a NumPy array representing the current image (h x w x 3 color channels)
+        ret, frame = cam.read()
+
+        # If reading fails, stop the loop
+        if not ret: 
+            print("Can't receive frame. Exiting....")
+            break
+
+        # Mirror the frame
+        frame = cv2.flip(frame, 1)
+
+
+        """
+        3. Prepare frames from OpenCV and process it for MediaPipe
+        """
+        # Prepare data
+        mp_image = mp.Image(image_format = mp.ImageFormat.SRGB, data=frame)
+
+
+
+        """
+        4. Run detectection and Show the handlandmark result
+        """
+        # Detect hand landmarks from the input real-time webcam
+        frame_timestamp = (int) (time.time() * 1000) #in millisecond
+        detector.detect_async(mp_image, frame_timestamp)
+
+
+
+        """
+        5. TO-DO 1: Visualize the result
+        """
+        
+
+
+        # Show the live webcam
+        cv2.imshow('Rock-Paper-Scissors Recognition', frame)
+
+        # Close the window
+        if cv2.waitKey(1) == ord('q'):
+            break
+        if cv2.getWindowProperty('Rock-Paper-Scissors Recognition', cv2.WND_PROP_VISIBLE) < 1:
+            break
+
+    cam.release()
+    cv2.destroyAllWindows()
+
+    """
+    TO-DO 2: Save hand landmarks as dataset
+    """
 
 if __name__ == "__main__":
     main()
